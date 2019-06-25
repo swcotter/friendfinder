de20d70eefa153f34024670cfeb77afe6742e393
Samuel Cotter <swcotter@comcast.net>
2019-06-24 20:54:06 -0700

    Reworked cars.js and added photos

---
+++ app/data/cars.js
@@ -0,0 +1,100 @@
+var cars = [
+    {
+        name: "Porsche 911 Turbo",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/3/3b/Porsche_911_Turbo_S_Exclusive_Series_IMG_0698.jpg",
+        scores: [
+            5,
+            3,
+            5,
+            3,
+            5,
+            5,
+            3,
+            3,
+            3,
+            5
+        ]
+    },
+    {
+        name: "Subaru WRX STi",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/8/86/04-05_Subaru_WRX_STi_2.jpg",
+        scores: [
+            1,
+            5,
+            3,
+            3,
+            3,
+            1,
+            3,
+            3,
+            3,
+            3
+        ]
+    },
+    {
+        name: "Chevrolet Camaro",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/1/11/Chevrolet_Camaro_2.0_Turbo.jpg",
+        scores: [
+            5,
+            1,
+            3,
+            5,
+            3,
+            3,
+            3,
+            3,
+            3,
+            5
+        ]
+    },
+    {
+        name: "Land Rover Range Rover",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/1/18/Land_Rover_Range_Rover_Autobiography_2016.jpg",
+        scores: [
+            3,
+            3,
+            5,
+            3,
+            3,
+            3,
+            5,
+            5,
+            3,
+            3
+        ]
+    },
+    {
+        name: "Mazda Miata",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/5/5e/2nd_Mazda_Miata_--_05-28-2011_2.jpg",
+        scores: [
+            2,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            5,
+            1
+        ]
+    },
+    {
+        name: "Mitsubishi Mirage",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/3/37/2015_Mitsubishi_Mirage_%28LA_MY15%29_LS_hatchback_%2822644970463%29.jpg",
+        scores: [
+            3,
+            3,
+            3,
+            3,
+            3,
+            5,
+            3,
+            3,
+            3,
+            1
+        ]
+    }
+];
+
+module.exports = cars;
